9a2f6ee51ab531aaac30599d5c6036c935e5f7a9
bbbbs17 <bs000317@naver.com>
2024-05-22 01:22:08 +0900

    spring + html

---
+++ src/main/java/webLogin/webLogin.java
@@ -0,0 +1,11 @@
+package webLogin;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class webLogin {
+    public static void main(String[] args) {
+        SpringApplication.run(webLogin.class, args);
+    }
+}
